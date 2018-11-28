e9ad9852a6d5e8098afae1318e939ed73c4d42b0
volkan <volkan510@gmail.com>
2018-11-29 00:12:11 +1100

    ok

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,55 +1,65 @@
 package hello;
 
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
 
 
 @RestController
-@RequestMapping("/api") 
+@RequestMapping(value= "/api" , method = RequestMethod.GET ) 
 public class GreetingController {
 
-	private final AtomicLong counter = new AtomicLong();
+	private final AtomicLong counter = new AtomicLong();	
 	
-	@RequestMapping("/")
-	public String welcome()
-	{
-		return "Welcome to RESTful web service";
-	}
-	
-	@RequestMapping("/greeting")
+	/*
+	@RequestMapping(value= "/greeting" , method = RequestMethod.GET ) 
 	public Greeting greeting(@RequestParam(value="name", defaultValue = "World")String name)
 	{
 		return new Greeting(counter.incrementAndGet(), "Hello " + name );
-	}
-	@RequestMapping("/Fibonacci")
-	public int fibonacci(@RequestParam(value="n" , defaultValue = "0")int n )
+	}*/
+	@ApiOperation( value = "Returns the nth fibonacci number.", 
+				notes = "Reverses the letters of each word in a sentence." ,
+				response = Long.class )
+	@RequestMapping(value= "/Fibonacci" , method = RequestMethod.GET ) 
+	public int Fibonacci(@RequestParam(value="n" , defaultValue = "0")int n )
 	{
 		FibonacciNumber fibonacci = new FibonacciNumber();
 		int result= fibonacci.fib( n );
 		return result;
 	}
 	
-	@RequestMapping("/ReverseWords")
-	public String reverse(@RequestParam(value="sentence" , defaultValue = "")String sentence)
+	@ApiOperation( value = "Reverses the letters of each word in a sentence" ,
+				   notes = "Reverses the letters of each word in a sentence." ,
+				   response = String.class)	
+	@RequestMapping(value= "/ReverseWords" , method = RequestMethod.GET ) 
+	public String ReverseWords(@RequestParam(value="sentence" , defaultValue = "")String sentence)
 	{		
 		Reversing reverse = new Reversing();
 		String result= reverse.reverse(sentence, "") ;
 		return result;
 	}
-	
-	@RequestMapping("/Token")
-	public String getToken()
+	@ApiOperation( value = "Your token." ,
+				   notes = "Your token." ,
+				   response = UUID.class)		
+	@RequestMapping(value= "/Token" , method = RequestMethod.GET ) 	
+	public String Token()
 	{				
 		return "https://join.readify.net/Status/1c3d1467-9a0f-49c2-bfc6-e30e99e827ef";
 	}
 	
-	@RequestMapping("/TriangleType")
-	public String TriangleType(@RequestParam(value="a" , defaultValue = "0")int a,@RequestParam(value="b" , defaultValue = "0")int b ,@RequestParam(value="c" , defaultValue = "0")int c)
+	
+	@ApiOperation( value = "Returns the type of triange given the lengths of its sides." ,
+				   notes= "Returns the type of triange given the lengths of its sides." ,
+				   response = Integer.class )			
+	@RequestMapping(value= "/TriangleType" , method = RequestMethod.GET ) 	
+	public String TriangleType(@RequestParam(value="a"  )int a,@RequestParam(value="b" )int b ,@RequestParam(value="c")int c)
 	{  
-		String result = "Error";
+		String result = "Scalene";
 		
 	    if (a <= 0 || b <= 0 || c <= 0) return "Error"; // added test
 	    if (a == b && b == c) return "EQUILATERAL";
